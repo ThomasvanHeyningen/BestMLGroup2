eec0c178a4778b8854cc09886aa21b9ff77ffd64
Bas Vb <bas.berkel@student.ru.nl>
2015-04-24 01:39:51 +0200

    A last commit for today, time for another submission

--- python/main.py
+++ python/main.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.cross_validation import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.preprocessing import LabelEncoder
@@ -22,20 +22,20 @@ def load_data(train_size=0.8, testdata=False):
     extratest=pd.read_csv('extratestfeatures.csv')
     vec = DictVectorizer()
 
-    #names_cat = []
+
     #for name in train.columns[1:-1]:
     #    names_cat.append(name)
-    #    print name, len(np.unique(train[name]))
+    #   print name, len(np.unique(train[name]))
 
     numerical_label = ['amount_tsh','gps_height','longitude','latitude','region_code','district_code','population','construction_year']
     #removed labels: num_private
-    extra_label=['funder_num','installer_num','wpt_name_num','basin_num','subvillage_num','region_num','lga_num','ward_num'
+    extra_label=['funder_num','installer_num','basin_num','region_num','lga_num','ward_num'
         ,'public_meeting_num','scheme_management_num','scheme_name_num','permit_num','extraction_type_num'
         ,'extraction_type_group_num','extraction_type_class_num','management_num','management_group_num','payment_num'
         ,'payment_type_num','water_quality_num','quality_group_num','quantity_num','quantity_group_num','source_num'
         ,'source_type_num','source_class_num','waterpoint_type_num','waterpoint_type_group_num'
         ,'month_recorded','age_of_pump','date_recorded_distance_days_20140101']
-    #removed labels: recorded_by_num, day_recorded, year_recorded
+    #removed labels: recorded_by_num, day_recorded, year_recorded, wpt_name_num, subvillage_num
     X_train_num=train[numerical_label]
     X_test_num=test[numerical_label]
     X_extratrain_num=extratrain[extra_label]
@@ -56,12 +56,12 @@ def load_data(train_size=0.8, testdata=False):
 
 
 def trainrf():
-    transformer = TfidfTransformer(norm='l2', smooth_idf=True, sublinear_tf=False, use_idf=True)
-    X_train, X_valid, y_train, y_valid = load_data(train_size=0.8, testdata=False)
+    X_train, X_valid, y_train, y_valid = load_data(train_size=0.999, testdata=False)
 
     # Number of trees, increase this to beat the benchmark ;)
-    n_estimators = 100
+    n_estimators = 1000
     clf = RandomForestClassifier(n_jobs=3, n_estimators=n_estimators, max_depth=23)
+    #clf=GradientBoostingClassifier(n_estimators=60, max_depth=10, max_features=20, min_samples_leaf=4,verbose=1, subsample=0.85) # 0.85 score
     print(" -- Start training.")
     clf.fit(X_train, y_train)
     print clf.feature_importances_
@@ -72,23 +72,24 @@ def trainrf():
     print classification_report(y_valid, y_pred)
     print accuracy_score(y_valid, y_pred)
 
-    ''' #code to print predictions, pretty useless without original labels
+    #code to print predictions, pretty useless without original labels
     predictdata=np.column_stack((X_valid, y_pred))
     print predictdata.shape
     print y_prob.shape
     predictdata=np.hstack((predictdata, y_prob))
+    predictdata=np.hstack((predictdata, y_prob2))
     predictdata=np.column_stack((predictdata, y_valid))
     filedata=pd.DataFrame(data=predictdata)
     filedata.to_csv('predictions.csv', index=False)
-    '''
+
     encoder = LabelEncoder()
     y_true = encoder.fit_transform(y_valid)
     assert (encoder.classes_ == clf.classes_).all()
 
 
-    return clf, encoder, transformer
+    return clf, encoder
 
-def make_submission(clf, encoder, transformer, path='my_submission.csv'):
+def make_submission(clf, encoder, path='my_submission.csv'):
     path = sys.argv[3] if len(sys.argv) > 3 else path
     X_test, ids = load_data(testdata=True)
 
@@ -105,8 +106,8 @@ def make_submission(clf, encoder, transformer, path='my_submission.csv'):
 
 def main():
     print(" - Start.")
-    model, encoder, transformer = trainrf()
-    #make_submission(model, encoder, transformer)
+    model, encoder = trainrf()
+    make_submission(model, encoder)
     print(" - Finished.")
 if __name__ == '__main__':
     start_time = time.time()

f6f63dc28465b45da423e93ca60613f4195f5e97
ReHarmen <prins_harmen@hotmail.com>
2015-05-12 22:17:45 +0200

    Now imputes train and test set separately. Possibly merge them

--- python/imputation.py
+++ python/imputation.py
@@ -3,18 +3,22 @@ import pandas as pd
 import Orange
 from sklearn.preprocessing import Imputer
 
+cols = ['longitude']#, 'latitude', 'num_private', 'population', 'construction_year']
+
 def setNaN(df):
-	cols = ['longitude']#, 'latitude', 'num_private', 'population', 'construction_year']
-	df[cols].replace(0, np.nan)
+	df[cols] = df[cols].replace(0, np.nan)
 	return df
 
 def impute(df):
 	print "Setting NaNs"
 	df = setNaN(df)
 	imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
-	df = pd.DataFrame(imp.fit_transform(df))
+	print "Imputing"
+	df[cols] = imp.fit_transform(df[cols])
 	return df
 
 if __name__ == "__main__":
 	data = impute(pd.read_csv('../data/trainset.csv'))
-	data.to_csv('../data/imp_trainset.csv')
+	data.to_csv('../data/imp_trainset.csv', index=False)
+	data = impute(pd.read_csv('../data/orig_testset.csv'))
+	data.to_csv('../data/imp_testset.csv', index=False)

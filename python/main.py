cc6223df073b965b27fb88c2dfdaa37daf2e02f0
Bas Vb <bas.berkel@student.ru.nl>
2015-05-27 15:05:06 +0200

    make path usable in unix

--- python/main.py
+++ python/main.py
@@ -32,6 +32,7 @@ def load_data(train_size=0.8, testdata=False):
     try:
         # Unix
         data_dir='../data/'
+
         train = pd.read_csv(data_dir + 'trainset.csv')
         trainlabels = pd.read_csv(data_dir + 'trainlabels.csv')
         test = pd.read_csv(data_dir + 'testset.csv')
@@ -40,6 +41,7 @@ def load_data(train_size=0.8, testdata=False):
     except IOError:
         # Windows
         data_dir='..\\data\\'
+        print ("linux failed")
         train = pd.read_csv(data_dir + 'trainset.csv')
         trainlabels = pd.read_csv(data_dir + 'trainlabels.csv')
         test = pd.read_csv(data_dir + 'testset.csv')

7eb2482f7bc44860a5c638a075ebd2dc0ba0f471
Bas Vb <bas.berkel@student.ru.nl>
2015-06-17 15:41:55 +0200

    data from Hans-Christiaan

--- python/main.py
+++ python/main.py
@@ -35,11 +35,14 @@ def load_data(train_size=0.8, testdata=False):
     try:
         # Unix
         data_dir=os.path.dirname(os.path.abspath('')) + '/data/'
+
         train = pd.read_csv(data_dir + 'Training_set_with_temperature.csv')
         trainlabels = pd.read_csv(data_dir + 'trainlabels.csv')
         test = pd.read_csv(data_dir + 'Test_set_with_temperature.csv')
         extratrain=pd.read_csv(data_dir + 'extratrainfeatures.csv')
         extratest=pd.read_csv(data_dir + 'extratestfeatures.csv')
+        orderedtrain=pd.read_csv(data_dir + 'train_ordered.csv')
+        orderedtest=pd.read_csv(data_dir + 'test_ordered.csv')
     except IOError:
         # Windows
         data_dir='..\\data\\'
@@ -49,6 +52,8 @@ def load_data(train_size=0.8, testdata=False):
         test = pd.read_csv(data_dir + 'Test_set_with_temperature.csv')
         extratrain=pd.read_csv(data_dir + 'extratrainfeatures.csv')
         extratest=pd.read_csv(data_dir + 'extratestfeatures.csv')
+        orderedtrain=pd.read_csv(data_dir + 'train_ordered.csv')
+        orderedtest=pd.read_csv(data_dir + 'test_ordered.csv')
 
     #for name in train.columns[1:-1]:
     #    names_cat.append(name)
@@ -62,12 +67,9 @@ def load_data(train_size=0.8, testdata=False):
     #,'Month 4','Month 5','Month 6','Month 7','Month 8','Month 9','Month 10','Month 11' ,'Month 12']
     #removed labels: num_private
 
-    extra_label=['basin_num','region_num','lga_num','ward_num'
+    extra_label=['region_num','lga_num','ward_num'
         ,'public_meeting_num','scheme_management_num','scheme_name_num','permit_num','extraction_type_num'
-        ,'extraction_type_group_num','extraction_type_class_num','management_num','management_group_num','payment_num'
-        ,'payment_type_num','water_quality_num','quantity_num','quantity_group_num','source_num'
-        ,'source_class_num','waterpoint_type_num','waterpoint_type_group_num'
-        ,'month_recorded','age_of_pump','date_recorded_distance_days_20140101'
+        ,'extraction_type_group_num' ,'month_recorded','age_of_pump','date_recorded_distance_days_20140101'
         ,'basin_freq','region_freq','lga_freq','ward_freq','scheme_name_freq', 'year_recorded'
         ,'funder_clean2_num','installer_clean2_num', 'funder_clean2_freq','installer_clean2_freq']
         #,'5_nearest_functional','5_nearest_need_repair','5_nearest_broken','10_nearest_functional' have to look at this, seems that validation data is leaking through
@@ -77,6 +79,13 @@ def load_data(train_size=0.8, testdata=False):
     #removed labels: recorded_by_num, day_recorded, wpt_name_num, subvillage_num, amount_tsh,'quality_group_num','source_type_num'
     #more removed: 'funder_num','installer_num', 'funder_freq','installer_freq'
     #after hand cleaning: 'funder_clean_num','installer_clean_num', 'funder_clean_freq','installer_clean_freq'
+    #'extraction_type_class_num','management_num','management_group_num','payment_num'
+    #    ,'payment_type_num','water_quality_num','quantity_num','quantity_group_num','source_num'
+    #    ,'source_class_num','waterpoint_type_num','waterpoint_type_group_num', 'basin_num'
+
+
+    ordered_label = ['extraction_type_class','management','management_group','payment','payment_type','water_quality'
+        ,'quantity','quantity_group','source','source_class','waterpoint_type','waterpoint_type_group', 'basin']
 
     #Processing the labels into the train and test sets.
     X_train_num=train[numerical_label]
@@ -84,18 +93,21 @@ def load_data(train_size=0.8, testdata=False):
     X_extratrain_num=extratrain[extra_label]
     X_extratest_num=extratest[extra_label]
 
-    Xtrain=np.hstack((X_train_num, X_extratrain_num))
-    Xtest=np.hstack((X_test_num, X_extratest_num))
+    X_orderedtrain=orderedtrain[ordered_label]
+    X_orderedtest=orderedtest[ordered_label]
+
+    Xtrain=np.hstack((X_train_num, X_extratrain_num, X_orderedtrain))
+    Xtest=np.hstack((X_test_num, X_extratest_num, X_orderedtest))
     trainset = np.column_stack((Xtrain,trainlabels['status_group']))
     X_train, X_valid, Y_train, Y_valid = train_test_split(
         trainset[:, 0:-1], trainset[:, -1], train_size=train_size
     , random_state=12)
 
     if testdata:
-        print("Testing set has {0[0]} rows and {0[1]} columns/features".format(test.shape))
+        print("Testing set has {0[0]} rows and {0[1]} columns/features".format(Xtest.shape))
         return (Xtest, test['id'])
     else:
-        print("Training set has {0[0]} rows and {0[1]} columns/features".format(train.shape))
+        print("Training set has {0[0]} rows and {0[1]} columns/features".format(Xtrain.shape))
         return(X_train.astype(float), X_valid.astype(float), Y_train.astype(str), Y_valid.astype(str))
 
 def trainclf():
@@ -148,7 +160,7 @@ def trainclf():
     '''
 
     # Normal RandomForestClassifier
-    clf = RandomForestClassifier(n_jobs=3, n_estimators=400, max_depth=18, random_state=60)
+    clf = RandomForestClassifier(n_jobs=3, n_estimators=400, max_depth=19, random_state=60)
     clf.fit(X_train, y_train)
     print('RFC 1 LogLoss {score}'.format(score=log_loss(y_valid, clf.predict_proba(X_valid))))
     print('RFC 1 accuracy {score}'.format(score=accuracy_score(y_valid, clf.predict(X_valid))))
